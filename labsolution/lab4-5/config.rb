b4575fa3d703474d3c74c1a879f787de520d449d
Steve Drucker <sdrucker@figleaf.com>
2015-07-25 11:50:02 -0400

    lab 4-5

--- labsolution/lab4-5/config.rb
+++ labsolution/lab4-5/config.rb
@@ -1,4 +1,6 @@
+
 require 'compass/import-once/activate'
+require 'sass_paths'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
@@ -7,10 +9,13 @@ css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
-output_style = :compressed
- 
+relative_assets = true
+
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
+
+SassPaths.append('mystuff')
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
